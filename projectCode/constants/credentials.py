d588bb33b3f17b17c5c5d689cb33a64c463d3820
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-06-06 21:54:34 +0200

    implemented vgg19

--- projectCode/constants/credentials.py
+++ projectCode/constants/credentials.py
@@ -1,15 +1,9 @@
 import os
 
 class credentials(object):
-<<<<<<< HEAD
-  FOLDR = ["dataset images", "../originalData"]
-=======
   FOLDR = "originalData"
->>>>>>> 581e7917a86cf25166838717a464f6f6d337afa9
   DATADIR = os.getcwd()+"\\"+ FOLDR[1]
   CATEGORIES = ['Closed_fist', 'Finger_guns', 'Open_palm', 'Peace_sign', 'Pinky', 'Pointing', 'Rocknroll', 'Spiderman', 'Spock', 'Thumbs_up']
   
-  isConstantSize = True # sets if image has constants size
-  # DOWNSIZE = 50 # current downsize if size is scalable, currently non-functioning
-  IMGSIZE = 100 # constant size to put images as
+  IMGSIZE = 128 # constant size to put images as
   
