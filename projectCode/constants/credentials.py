b219c59547b1b05bc127f5a3e4f836997634698e
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-06-07 16:26:35 +0200

    fully integrated project values in resize script

--- projectCode/constants/credentials.py
+++ projectCode/constants/credentials.py
@@ -3,4 +3,5 @@ class credentials(object):
   FOLDR = ["resizedData","originalData"]
   DIR = PATH + FOLDR[0]
   CATEGORIES = ['Closed_fist', 'Finger_guns', 'Open_palm', 'Peace_sign', 'Pinky', 'Pointing', 'Rocknroll', 'Spiderman', 'Spock', 'Thumbs_up']
+  IMGRESIZETO = 500 # constant size to resize from original data
   IMGSIZE = 250 # constant size to put images as
--- projectCode/resize.py
+++ projectCode/resize.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 from PIL import Image
-import os, sys
+import os
 from constants.credentials import credentials as CR
 
-path = "./originalData/"
-savePath = "./resizedData/"
+path = "./"+CR.FOLDR[1]+"/"
+savePath = "./"+CR.FOLDR[0]+"/"
 
-imgSizeTo = 500
+imgSizeTo = CR.IMGRESIZETO
 
 def resize(saveFrom, saveTo, size):
     dirs = os.listdir( saveFrom )
@@ -14,11 +14,13 @@ def resize(saveFrom, saveTo, size):
         if os.path.isfile(saveFrom+item):
             im = Image.open(saveFrom+item)
             f, e = os.path.splitext(saveTo+item)
-            imResize = im.resize((size,size), Image.ANTIALIAS).rotate(270)
+            imResize = im.resize((size,size), Image.Resampling.LANCZOS).rotate(270)
             imResize.save(f + '_resized.jpg', 'JPEG', quality=90)
+    print("RESIZEDATA::done!")
 
 
 for foldr in CR.CATEGORIES:
+  print("RESIZEDATA::resizing for "+foldr+"...")
   dataFrom = path + foldr +"/"
   dataTo = savePath + foldr +"/"
   resize(dataFrom, dataTo, imgSizeTo)
